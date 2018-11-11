eacf0612cf4773d752f9b25865849cc6fdcc1cd2
dimaLytvyniuk <dima20-12@outlook.com>
2018-11-11 19:44:24 +0200

    Use common Utilites package

--- EnglishLearning.TaskService.Application/InternalServices/FilterEnglishTaskService.cs
+++ EnglishLearning.TaskService.Application/InternalServices/FilterEnglishTaskService.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
-using EnglishLearning.TaskService.Infrastructure;
-using EnglishLearning.TaskService.Infrastructure.ExpressionHelpers;
-using EnglishLearning.TaskService.Infrastructure.Extensions;
 using EnglishLearning.TaskService.Persistence.Abstract;
 using EnglishLearning.TaskService.Persistence.Entities;
+using EnglishLearning.Utilities.Enums;
+using EnglishLearning.Utilities.Linq.Extensions;
+using EnglishLearning.Utilities.Expressions.Helpers;
 
 namespace EnglishLearning.TaskService.Application.InternalServices
 {
@@ -38,7 +38,7 @@ namespace EnglishLearning.TaskService.Application.InternalServices
 
             if (!taskTypes.IsNullOrEmpty())
             {
-                var enumTaskTypes = EnumHelpers.ConverToEnumArray<TaskType>(taskTypes);
+                var enumTaskTypes = EnumConverter.ConverToEnumArray<TaskType>(taskTypes);
                 
                 var taskTypeExpression = new List<Expression<Func<EnglishTask, bool>>>();
 
@@ -53,7 +53,7 @@ namespace EnglishLearning.TaskService.Application.InternalServices
 
             if (!grammarParts.IsNullOrEmpty())
             {
-                var enumGrammarParts = EnumHelpers.ConverToEnumArray<GrammarPart>(grammarParts);
+                var enumGrammarParts = EnumConverter.ConverToEnumArray<GrammarPart>(grammarParts);
                 
                 var grammarTypeExpressions = new List<Expression<Func<EnglishTask, bool>>>();
 
@@ -73,7 +73,7 @@ namespace EnglishLearning.TaskService.Application.InternalServices
 
             if (!englishLevels.IsNullOrEmpty())
             {
-                var enumEnglishLevels = EnumHelpers.ConverToEnumArray<EnglishLevel>(englishLevels);
+                var enumEnglishLevels = EnumConverter.ConverToEnumArray<EnglishLevel>(englishLevels);
                 
                 var englishLevelExpressions = new List<Expression<Func<EnglishTask, bool>>>();
 
