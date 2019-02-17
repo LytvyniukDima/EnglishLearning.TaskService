38333b0e3d824707d089d120016c1efab09c1d0a
dimaLytvyniuk <dima20-12@outlook.com>
2019-02-17 17:53:57 +0200

    Add killing container before running container

--- EnvironmentTools/runService.sh
+++ EnvironmentTools/runService.sh
@@ -2,6 +2,7 @@ imageName="english_tasks"
 containerName="task_service"
 networkName="english-net"
 
+docker kill $containerName
 docker rm $containerName
 
 docker run -p 8000:8000 --name $containerName --network $networkName $imageName
