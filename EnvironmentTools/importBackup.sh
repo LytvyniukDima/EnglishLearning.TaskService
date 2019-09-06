5dd33123d20d4439989499ae273183d8b03688b5
LytvyniukDima <dima.lytvyniuk@gmail.com>
2019-09-06 13:14:24 +0300

    Update backups

--- EnvironmentTools/importBackup.sh
+++ EnvironmentTools/importBackup.sh
@@ -1,1 +1,2 @@
 mongoimport --db EnglishLearning_TaskService --collection EnglishTasks --drop EnglishTasks.json
+mongoimport --db EnglishLearning_TaskService --collection UserInformation --drop UserInformation.json
