06e07ed535511c4ccc83d33c9501bb53f93589e0
LytvyniukDima <dima.lytvyniuk@gmail.com>
2019-09-16 14:29:32 +0300

    Add aggregate service

---
+++ EnglishLearning.TaskService.Application/Abstract/IStoredTaskInformationAggregateService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using EnglishLearning.TaskService.Application.Models.Filtering;
+
+namespace EnglishLearning.TaskService.Application.Abstract
+{
+    public interface IStoredTaskInformationAggregateService
+    {
+        Task<StoredTasksInformationAggregate> GetStoredTaskInformationAggregate();
+    }
+}
---
+++ EnglishLearning.TaskService.Application/Services/StoredTaskInformationAggregateService.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using EnglishLearning.TaskService.Application.Abstract;
+using EnglishLearning.TaskService.Application.Models.Filtering;
+
+namespace EnglishLearning.TaskService.Application.Services
+{
+    public class StoredTaskInformationAggregateService : IStoredTaskInformationAggregateService
+    {
+        private readonly IEnglishTaskFilterOptionsService _filterOptionsService;
+
+        public StoredTaskInformationAggregateService(IEnglishTaskFilterOptionsService filterOptionsService)
+        {
+            _filterOptionsService = filterOptionsService;
+        }
+        
+        public async Task<StoredTasksInformationAggregate> GetStoredTaskInformationAggregate()
+        {
+            var taskInformationModels = await _filterOptionsService.GetTaskInformationModels();
+            var fullFilter = await _filterOptionsService.GetFullFilter();
+
+            var taskInformationModelsDictionary = taskInformationModels.ToDictionary(x => x.EnglishLevel);
+            var grammarParts = fullFilter.GrammarPartFilterOptions.Keys.ToList();
+            var aggregate = new StoredTasksInformationAggregate(taskInformationModelsDictionary, grammarParts);
+
+            return aggregate;
+        }
+    }
+}
