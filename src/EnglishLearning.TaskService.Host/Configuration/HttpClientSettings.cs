027ab207896a8ce39df3f0125d16489de75a6624
LytvyniukDima <dima.lytvyniuk@gmail.com>
2021-03-14 21:12:39 +0200

    feat: setup word metadata client

---
+++ src/EnglishLearning.TaskService.Host/Configuration/HttpClientSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using EnglishLearning.Dictionary.Client;
+using EnglishLearning.Utilities.Identity.DelegationHandlers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EnglishLearning.TaskService.Host.Configuration
+{
+    public static class HttpClientSettings
+    {
+        public static IServiceCollection AddEnglishLearningHttp(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var dictionaryAddress = configuration
+                .GetValue<Uri>("ExternalServices:Dictionary");
+
+            services
+                .AddHttpClient<WordMetadataClient>(c =>
+                {
+                    c.BaseAddress = dictionaryAddress;
+                })
+                .AddHttpMessageHandler<JwtInfoHeaderHandler>();
+            
+            return services;
+        }
+    }
+}
